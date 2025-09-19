cee73c4fe1f4b03427c97f122f643034cb5fedb1
Rodrigo M <rodrigo@gmail.com>
2025-09-19 16:37:37 -0300

    adding new transformations

---
+++ models/dates/date-accessor-transformation.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+# Mock data
+data = {
+    'merchant_id': ['m1', 'm1', 'm2', 'm2', 'm3'],
+    'date': ['2013-01-04', '2013-01-05', '2013-01-08', '2013-01-09', '2013-01-10'],
+    'count_of_events': [100, 200, 300, 400, 500],
+    'usd_amount': [50.0, 100.0, 150.0, 200.0, 250.0]
+}
+df = pd.DataFrame(data)
+print("Original DataFrame:")
+print(df)
+
+df['year'] = df['date'].dt.year
+df['month'] = df['date'].dt.month
+df['day'] = df['date'].dt.day
+print('\n. After extractiong components:')
+print(df)
